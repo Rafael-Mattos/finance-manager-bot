c9678e53e0d3855ce7c367f34b3b660578415fe2
Rafael <rafael.rmtt@gmail.com>
2025-06-01 20:21:18 -0300

    jwt ok

--- api/core/urls.py
+++ api/core/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/v1/', include('transactions.urls')),
+    path('api/v1/token/', jwt_views.TokenObtainPairView.as_view(),
+		name='token_obtain_pair'),
+    path('api/v1/token/refresh/', jwt_views.TokenRefreshView.as_view(),
+		name='token_refresh'),
+    path('api/v1/token/verify/', jwt_views.TokenVerifyView.as_view(), 
+         name='token_verify'), # Verifica validade access token e refresh token
 ]
