8058d76636bcae8fdc7e530f34a00b158b251552
Rafael <rafael.rmtt@gmail.com>
2025-07-20 19:21:34 -0300

    api_requests ok

--- bot/api_requests.py
+++ bot/api_requests.py
@@ -44,12 +44,99 @@ class ApiRequests:
             return False
 
         return False
+    
+
+    def get_all_categories(self):
+        url = f'{self.DOMAIN}/api/v1/categories/'
+
+        response = requests.get(url, headers=self.get_headers())
+        return response.json()
+
+
+    def insert_category(self, category, is_expense: bool):
+        if not isinstance(is_expense, bool):
+            return {'error': 'O valor passado para o parâmetro "is_expense" deve ser do tipo bool.'}
+        
+        url = f'{self.DOMAIN}/api/v1/categories/'
+        info = {
+            'name': category,
+            'type': 'e' if is_expense else 'r'
+        }
+
+        response = requests.post(url, headers=self.get_headers(), json=info)
+        return response.json()
+    
+
+    def patch_category(self, id, category):
+        url = f'{self.DOMAIN}/api/v1/categories/{id}/'
+        info = {
+            'name': category
+        }
+
+        response = requests.patch(url, headers=self.get_headers(), json=info)
+        return response.json()
+    
+
+    def delete_category(self, id):
+        url = f'{self.DOMAIN}/api/v1/categories/{id}/'
+
+        response = requests.delete(url, headers=self.get_headers())
+
+        if response.status_code == 204:
+            return {"detail": "Categoria deletada com sucesso."}
+        elif response.status_code == 500:
+            return {
+                "detail": "Exclusão não é permitida. Verifique se existem descrições atreladas a esta categoria."
+            }
+        return {"error": "Nenhuma categoria encontrada."}
 
 
     def get_top_descriptions(self):
         url = f'{self.DOMAIN}/api/v1/transactions/top-descriptions/'
         response = requests.get(url, headers=self.get_headers())
         return response.json()
+    
+
+    def get_all_descriptions(self):
+        url = f'{self.DOMAIN}/api/v1/descriptions/'
+
+        response = requests.get(url, headers=self.get_headers())
+        return response.json()
+    
+
+    def insert_description(self, category_id, description):
+        url = f'{self.DOMAIN}/api/v1/descriptions/'
+        info = {
+            'category': category_id,
+            'name': description
+        }
+
+        response = requests.post(url, headers=self.get_headers(), json=info)
+        return response.json()
+    
+
+    def put_description(self, id, category_id, description):
+        url = f'{self.DOMAIN}/api/v1/descriptions/{id}/'
+        info = {
+            'category': category_id,
+            'name': description
+        }
+
+        response = requests.put(url, headers=self.get_headers(), json=info)
+        return response.json()
+    
+
+    def delete_description(self, id):
+        url = f'{self.DOMAIN}/api/v1/descriptions/{id}/'
+        response = requests.delete(url, headers=self.get_headers())
+
+        if response.status_code == 204:
+            return {"detail": "Descrição deletada com sucesso."}
+        elif response.status_code == 500:
+            return {
+                "detail": "Exclusão não é permitida. Verifique se existem transações atreladas a esta descrição."
+            }
+        return {"error": "Nenhuma descrição encontrada."}
     
 
     def insert_transaction(self, category, description, amount, date=None, obs=None):
@@ -72,6 +159,23 @@ class ApiRequests:
     def get_transaction(self, id):
         url = f'{self.DOMAIN}/api/v1/transactions/{id}/'
         response = requests.get(url, headers=self.get_headers())
+        return response.json()
+    
+
+    def get_transaction_by_condition(self, rql_condition):
+        url = f'{self.DOMAIN}/api/v1/transactions/?{rql_condition}'
+        response = requests.get(url, headers=self.get_headers())
+        return response.json()
+    
+
+    def patch_transaction(self, id, key, value):
+        url = f'{self.DOMAIN}/api/v1/transactions/{id}/'
+
+        info = {
+            key: value
+        }
+
+        response = requests.patch(url, headers=self.get_headers(), json=info)
         return response.json()
     
 
@@ -111,11 +215,3 @@ class ApiRequests:
         }
         response = requests.delete(url, headers=self.get_headers(), json=info)
         return response.json()
-
-
-if __name__ == '__main__':
-    teste = ApiRequests()
-    # print(teste.insert_transaction(3, 2, 200.57))
-    print(teste.delete_transaction(4948))
-    # print(teste.insert_recurring_transaction(1, 1, 70000, '2025-09-23', repeat=4))
-    # print(teste.delete_recurring_transactions(4900, '2025-08-01'))
